be792756979ac3a8ba89bcdd5b55904ef4a7e8af
Afnan Asilah <afnanyogs@gmail.com>
2024-05-11 12:02:17 +0700

    menambahkan funsi int

--- stackusinglinkedlis_0153/stackusinglinkedlis_0153.cpp
+++ stackusinglinkedlis_0153/stackusinglinkedlis_0153.cpp
@@ -40,25 +40,30 @@ void pop()
 	if (isEmpty());
 
 	{
-     cout << "Stack is empty." << endl;
-	 top = top->next;
+		cout << "Stack is empty." << endl;
+		top = top->next;
 	}
-	 cout << "Popped value: " << top->data << endl;
-	 top = top->next;
+	cout << "Popped value: " << top->data << endl;
+	top = top->next;
 
-	 void peek()
-	 {
-		 if (top == NULL)
-		 {
-			 cout << "List is empty." << endl;
-		 }
-		 else {
-			 Node* current = top;
-			 while (current != NULL
-				 {
-					   cout << current->data << " " << endl;
+	void peek()
+	{
+		if (top == NULL)
+		{
+			cout << "List is empty." << endl;
+		}
+		else {
+			Node* current = top;
+			while (current != NULL
+				{
+					  cout << current->data << " " << endl;
+					  current = current->next;
+				}
+			cout << endl;
+		}
+	};
 
-};
+	bool isEmpty()
 iint main();
  {
 
