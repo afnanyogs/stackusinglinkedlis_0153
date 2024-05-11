48971db866d12e03545d3f4879b36e79c7a1538e
Afnan Asilah <afnanyogs@gmail.com>
2024-05-11 12:25:16 +0700

    selesai

--- stackusinglinkedlis_0153/stackusinglinkedlis_0153.cpp
+++ stackusinglinkedlis_0153/stackusinglinkedlis_0153.cpp
@@ -4,102 +4,119 @@ using namespace std;
 class Node
 {
 public:
-	int data;
-	Node* next;
+    int data;
+    Node* next;
 
-	Node()
-	{
-		next = NULL;
-	}
-	
+    Node()
+    {
+        next = NULL;
+    }
 };
 
 class stack
 {
 private:
-	Node* top;
+    Node* top;
 
 public:
-	stack()
-	{
-		top = NULL;
-	}
+    stack()
+    {
+        top = NULL;
+    }
+    int push(int value)
+    {
 
-int push(int value)
-{
-	Node* newNode = new Node();
-	newNode->data = value;
-	newNode->next = top;
-	top = newNode;
-	cout << "Push value: " << value << endl;
-	return value;
-}
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
 
-void pop()
-{
-	if (isEmpty());
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty." << endl;
+            return;
+        }
+        cout << "Popped value: " << top->data << endl;
+        top = top->next;
+    }
 
-	{
-		cout << "Stack is empty." << endl;
-		top = top->next;
-	}
-	cout << "Popped value: " << top->data << endl;
-	top = top->next;
+    void peek()
+    {
+        if (top == NULL)
+        {
+            cout << "List is empty." << endl;
+        }
+        else {
+            Node* current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
 
-	void peek()
-	{
-		if (top == NULL)
-		{
-			cout << "List is empty." << endl;
-		}
-		else {
-			Node* current = top;
-			while (current != NULL
-				{
-					  cout << current->data << " " << endl;
-					  current = current->next;
-				}
-			cout << endl;
-		}
-	};
-
-	bool isEmpty()
-	{
-		return top == NULL;
-	}
+    bool isEmpty()
+    {
+        return top == NULL;
+    }
 };
 
+
 int main()
- {
-	stack stack;
-	int choice = 0;
- int value;
+{
+    stack stack;
 
- while (choice != 5)
- {
-	 cout << "1. Push\n";
-	 cout << "2. Pop\n";
-	 cout << "3. Peek\n";
-	 cout << "4. Exit\n";
-	 cout << "Enter your choice: \n";
-	 cin >> choice;
 
-	 switch (choice)
-	 {
-	 case 1:
-		 cout << "Enter the value to push: ";
-		 cin >> value;
-		 stack.push(value);
-		 break;
-	 case 2:
-		 if (!stack.isEmpty()) {
-			 stack.pop();
-		 }
-		 else {
-			 cout << "Stack is empty. Cannot pop" << endl;
-		 } break;
-	 case 3:
-		 if (!stack.isEmpty()) {
-			 stack.peek();
+    int choice = 0;
+    int value;
 
- }
+    while (choice != 5)
+    {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: \n";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value);
+            break;
+        case 2:
+            if (!stack.isEmpty()) {
+                stack.pop();
+            }
+            else {
+                cout << "Stack is empty. Cannot pop" << endl;
+            }
+            break;
+        case 3:
+            if (!stack.isEmpty()) {
+                stack.peek();
+            }
+            else {
+                cout << "Stack is empty. No top value" << endl;
+            }
+            break;
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
+        default:
+            cout << "Invalid choice. Try again." << endl;
+            break;
+        }
+        cout << endl;
+    }
+    return 0;
+}
